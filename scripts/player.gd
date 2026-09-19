07efd2f01c5102b9c314a7b376aa57636bc2b309
Ardhi Purna <ardhipurna11@gmail.com>
2026-09-19 18:58:48 +0800

    Add player walking animation system

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
+var facing_direction =  "down"
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector(
@@ -12,12 +13,25 @@ func _physics_process(delta: float) -> void:
 	
 	if direction != Vector2.ZERO:
 		if direction.x > 0:
-			$AnimatedSprite2D.play("idle_right")
+			facing_direction = "right"
+			$AnimatedSprite2D.play("walk_right")
 		elif direction.x < 0: 
-			$AnimatedSprite2D.play("idle_left")
+			facing_direction = "left"
+			$AnimatedSprite2D.play("walk_left")
 		elif direction.y > 0:
-			$AnimatedSprite2D.play("idle_down")
+			facing_direction = "down"
+			$AnimatedSprite2D.play("walk_down")
 		elif  direction.y < 0:
+			facing_direction = "up"
+			$AnimatedSprite2D.play("walk_up")
+	else:
+		if facing_direction == "right":
+			$AnimatedSprite2D.play("idle_right")
+		elif facing_direction == "left": 
+			$AnimatedSprite2D.play("idle_left")
+		elif facing_direction == "down":
+			$AnimatedSprite2D.play("idle_down")
+		elif facing_direction == "up":
 			$AnimatedSprite2D.play("idle_up")
 	
 	velocity = SPEED * direction
