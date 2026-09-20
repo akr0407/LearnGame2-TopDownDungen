08ca240eac4d1aeea0cc50387643a681d61b289a
Ardhi Purna <ardhipurna11@gmail.com>
2026-09-20 16:33:01 +0800

    Add enemy health and death system

---
+++ scripts/enemy.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+const SPEED = 80.0
+var facing_direction = "down"
+var health = 40
+
+func take_damage(amount) -> void:
+	health -= amount
+	
+	if health <= 0:
+		queue_free()
+	
+
+func _physics_process(_delta: float) -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	var distance = position.distance_to(player.position)
+	
+	if distance > 30:
+		var direction = position.direction_to(player.position)
+		
+		velocity = SPEED * direction
+		
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				facing_direction = "right"
+				$AnimatedSprite2D.play("walk_right")
+			else:
+				facing_direction = "left"
+				$AnimatedSprite2D.play("walk_left")
+		else:
+			if direction.y > 0:
+				facing_direction = "down"
+				$AnimatedSprite2D.play("walk_down")
+			else:
+				facing_direction = "up"
+				$AnimatedSprite2D.play("walk_up")
+	else:
+		velocity = Vector2.ZERO
+		
+		if facing_direction == "up":
+			$AnimatedSprite2D.play("idle_up")
+		elif facing_direction == "down":
+			$AnimatedSprite2D.play("idle_down")
+		elif facing_direction == "right":
+			$AnimatedSprite2D.play("idle_right")
+		elif facing_direction == "left":
+			$AnimatedSprite2D.play("idle_left")
+		else:
+			$AnimatedSprite2D.play("idle_down")
+		
+	move_and_slide()
