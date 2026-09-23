e7172e12c69a809177180cab161f758c8f6a471a
Ardhi Purna <ardhipurna11@gmail.com>
2026-09-23 15:25:44 +0800

    Add lever controlled door transition

--- scripts/door.gd
+++ scripts/door.gd
@@ -5,27 +5,37 @@ extends StaticBody2D
 var is_open = false
 
 func _on_interaction_area_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("player"):
+		return
+
 	print("Player is at the door")
 
-	if body.is_in_group("player") and not is_open:
-		print("Player detected!")
-		is_open = true
-		if body.has_key == true:
-			$Sprite2D.visible = false
-			$CollisionShape2D.set_deferred("disabled", true)
-			body.has_key = false
-			if destination:
-				body.position = destination.position
-		else:
-			print("Player dont have key")
+	if is_open:
+		if destination:
+			body.position = destination.position
+		return
 
-func open_door() -> void:
+	if body.has_key:
+		is_open = true
+		$Sprite2D.visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
+
+		body.has_key = false
+
+		if destination:
+			body.position = destination.position
+	else:
+		print("Player doesn't have key")
+		
+func open_door(body: Node2D = null) -> void:
 	if is_open:
 		return
-		
+
 	is_open = true
-	
 	$Sprite2D.visible = false
 	$CollisionShape2D.set_deferred("disabled", true)
-	
-	print("Door opened")
+
+	print("Door opened!")
+
+	if body and destination:
+		body.position = destination.position
